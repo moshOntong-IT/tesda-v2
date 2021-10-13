e26a2d410031c9cc6b935bf043d689682040ccc1
moshOntong-IT <moshontong@gmail.com>
2021-10-13 16:20:32 +0800

    add sql statement

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const connection = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.get('/api', function(req,res){
-    connection.query("SELECT * from assets", function (err,result){
-        res.send(result)
+    connection.query(`SELECT * from assets a LEFT JOIN departmentlocations dl ON dl.ID = a.DepartmentLocationID 
+    LEFT JOIN departments d ON d.ID =dl.DepartmentID`, function (err,result){
+        res.json(result)
     });
 });
 
